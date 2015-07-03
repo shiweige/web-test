3175e27423848e20df27ff4f59299aa927527776
shiweige <shiwei@vessel.com>
2015-07-03 15:35:10 +0800

    add Hello.java

---
+++ src/main/java/com/vessel/test/Hello.java
@@ -0,0 +1,10 @@
+package com.vessel.test;
+
+/**
+ * Created by langpai on 15/7/3.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
